9bb773aa136e8f246e1ceb983e0c75ef334b7f9c
rolivasknapton <55037360+rolivasknapton@users.noreply.github.com>
2024-02-16 18:19:18 -0600

    decrease area of missle spawner

--- Assets/Scripts/DecreaseSizeOfMissleSpawner.cs
+++ Assets/Scripts/DecreaseSizeOfMissleSpawner.cs
@@ -2,15 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DecreaseSizeOfMissleSpawner : MonoBehaviour
 {
-    private Collider missleSpawnerArea;
+    public BoxCollider missleSpawnerArea;
+
+    public float lerpedValue;
+    public float decreaseAreaDuration = 3;
     private void Awake()
     {
-        missleSpawnerArea = this.GetComponent<BoxCollider>();
+        
     }
-    private void Update()
+    private void Start()
     {
-        //missleSpawnerArea.
+        missleSpawnerArea = this.GetComponent<BoxCollider>();
+        
+
+        StartCoroutine(DecreaseArea(70,1));
+    }
+    IEnumerator DecreaseArea(float startValue, float endValue)
+    {
+        float timeElapsed = 0;
+
+        while (timeElapsed < decreaseAreaDuration)
+        {
+            lerpedValue = Mathf.Lerp(startValue, endValue, timeElapsed / decreaseAreaDuration);
+            missleSpawnerArea.size = new Vector3(lerpedValue, 22.56f, lerpedValue);
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        lerpedValue = endValue;
     }
 }
