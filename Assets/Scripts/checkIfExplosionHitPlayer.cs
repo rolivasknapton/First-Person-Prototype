308471034025e93ad92ed5f70ecbfb6ef2512c34
rolivasknapton <55037360+rolivasknapton@users.noreply.github.com>
2024-02-27 08:47:10 -0600

    death screen

--- Assets/Scripts/checkIfExplosionHitPlayer.cs
+++ Assets/Scripts/checkIfExplosionHitPlayer.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class checkIfExplosionHitPlayer : MonoBehaviour
 {
+    public GameObject deathscreen;
+    private Image deathimage;
+    private void Start()
+    {
+        deathscreen = GameObject.Find("DeathScreen");
+        deathimage = deathscreen.GetComponent<Image>();
+
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("explosion"))
         {
             Debug.Log("Hit by explosion");
+            deathimage.enabled = true;
             // You can add more logic here if needed
         }
         else
